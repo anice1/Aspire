e450019d439d9f34062cb82c1e7a7ed3056ee95a
agjohngabriel <agjohngabriel@gmail.com>
2020-09-05 00:40:36 -0400

    Remember those walls I built

--- app/Http/Controllers/frontend/DashboardController.php
+++ app/Http/Controllers/frontend/DashboardController.php
@@ -19,7 +19,6 @@ class DashboardController extends Controller
         }elseif(Auth::user()->hasRole('student'))
         {
             return view('frontend.student.index');
-<<<<<<< HEAD
 
         }elseif(Auth::user()->hasRole('teacher'))
         {
@@ -33,13 +32,14 @@ class DashboardController extends Controller
 
         }else
         {
-            return "Out of range";
-=======
-        }elseif(Auth::user()->hasRole('teacher')){
-            return view('frontend.Teacher.index');
-        }else{
-            return "i Dont understand the logic bro";
->>>>>>> origin/shieldcode
+            return "Remember those walls I built
+                    Well, baby, they're tumbling down
+And they didn't even put up a fight
+They didn't even make a sound
+I found a way to let you win
+But I never really had a doubt
+Standing in the light of your halo
+I got my angel now";
         }
     }
 }
